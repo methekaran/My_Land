82d91b04220b6a91e7a1eec0d24689b1cf59a8c4
methekaran <92954893+methekaran@users.noreply.github.com>
2024-02-01 07:36:51 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -26,17 +26,20 @@ continue_op = ""
 
 #create a dictionary with key as operation and value is the function names
 my_dict = {"+": addition, "-": subtraction, "*": multiplication, "/": division}
-inp_1 = float(input(f"Please enter the first number="))
 
-while (continue_op != "n"):
-    op = input(f"Please enter the operation type:")
-    inp_2 = float(input(f"Please enter the second number="))
-    function_op = my_dict[op]
-    print(f"{inp_1} {op} {inp_2}={function_op(inp_1,inp_2)}\n")
-    result = function_op(inp_1, inp_2)
-    continue_op = input(
-        "Do you want to continue on the previous result type y or n:")
-    if (continue_op == "y"):
-        inp_1 = result
+def calculator():
+    inp_1 = float(input(f"Please enter the first number="))
+    while (continue_op != "n"):
+        op = input(f"Please enter the operation type:")
+        inp_2 = float(input(f"Please enter the second number="))
+        function_op = my_dict[op]
+        print(f"{inp_1} {op} {inp_2}={function_op(inp_1,inp_2)}\n")
+        result = function_op(inp_1, inp_2)
+        continue_op = input("Do you want to continue on the previous result type y or n:")
+        if (continue_op == "y"):
+            inp_1 = result
+        else :
+            print("Start a new operation!!!")
+            calculator() ## recursive operation
 
-print(f"Good Bye!!!")
+calculator()
