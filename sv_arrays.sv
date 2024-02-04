01d7fafae7dfb6cd4dad392e7c57c6dc44071c15
methekaran <92954893+methekaran@users.noreply.github.com>
2024-02-04 08:17:18 -0800

    Update sv_arrays.sv

--- sv_arrays.sv
+++ sv_arrays.sv
@@ -1,6 +1,8 @@
 // Code your testbench here
 // or browse Examples
 
+//In general Queue's are better with the rich functions for accessing elements from an array 
+// Only associative and queue's have ability to delete a particular elements form an array
 
 module tb_top;
   
@@ -24,6 +26,13 @@ module tb_top;
     foreach(d_dyn[a])begin
       $display("After adding more elements:a=%d && value=%d",a,d_dyn[a]);
     end
+    
+    // delete an element from a dynamic array
+    //d_dyn.delete(10); //10 is an index here
+    //The above code is not possible for dynamic arrays only queue elements can be deleted one at a time.
+    //This will delete an entire array
+    d_dyn.delete();
+    $display("Size of the dynamic array is :%d",d_dyn.size());
   end
   
   initial begin
@@ -43,4 +52,30 @@ module tb_top;
     end
   end
   
+  // associative arrays
+  int my_assoc_arr[int];
+  int idx,a; 
+  initial begin
+    
+    repeat(5)begin
+      my_assoc_arr[idx]=idx*15;
+      idx++;
+    end
+    
+    //exists for an index
+    if(my_assoc_arr.exists(4))begin
+      $display("Array element found at 4 : :%d!!!",my_assoc_arr[4]);
+    end
+    
+    idx=0;
+    
+    do 
+      begin
+      $display("ASSOC_ARRAY value=%d && index=%d",my_assoc_arr[idx],idx);
+      a = idx;
+    end while(my_assoc_arr.next(idx));
+  end//initial
+    
+  
 endmodule : tb_top
+
