089c0c96008cdad526ab851ece862fcd357e498d
methekaran <92954893+methekaran@users.noreply.github.com>
2024-02-06 20:07:59 -0800

    Update stock_market.sv

--- stock_market.sv
+++ stock_market.sv
@@ -1,12 +1,35 @@
 module tb_top;
   int inp_arr[5] = {1,7,3,2,4};
+
+   int inp_arr[6] = {7,1,5,3,2,4};
   
   //Algorithm : set a buy price as a flag and sell price as a flag and then compare the each day's difference to see which is largest profit
-  int buy_price=100000;
+  int buy_price=1000;
   int my_assoc_arr[int];
   int idx;
   int max_q[$];
+  
+  int left_ptr=0;
+  int right_ptr=1;
+  int max_value[$];
+
+  //Time complexity O(n)
+  initial begin
+    while (right_ptr!=$size(inp_arr))begin
+      if(inp_arr[left_ptr] < inp_arr[right_ptr])begin
+        max_q.push_back(inp_arr[right_ptr] - inp_arr[left_ptr]);
+      end
+      else begin
+        left_ptr = right_ptr;
+      end
+      right_ptr++;
+    end
+    max_value = max_q.max();
+    $display("Max value is :%d",max_value[0]);
+  end
+  
  
+  //Below is the O(n^2)
  initial begin
   for( int i=0;i<$size(inp_arr);i++)begin
     if(inp_arr[i] < buy_price)begin
