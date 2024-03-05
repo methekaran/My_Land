767769539df577024bdc8b4a4c5749a12494004e
methekaran <92954893+methekaran@users.noreply.github.com>
2024-03-04 20:58:57 -0800

    Update pure_v_class.sv

--- pure_v_class.sv
+++ pure_v_class.sv
@@ -8,3 +8,16 @@ class my_ext_class extends my_pure_vc;
     $display("This is defined under child class");
   endfunction : display
 endclass : my_ext_class
+
+module my_mod;
+  //creating pure virtual class object is not allowed
+  // my_pure_vc pure_vc;
+  //accessing the pur virtual method is only allowed through the child or subclass
+  my_ext_class e_class;
+
+  initial begin
+    e_class = new();
+    e_class.display();
+  end
+  
+endmodule : my_mod
