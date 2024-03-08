e49ac9728919a7169153f9ff47c10ec07c4b30e8
methekaran <92954893+methekaran@users.noreply.github.com>
2024-03-08 09:10:08 -0800

    Update unique_100_numbers.sv

--- unique_100_numbers.sv
+++ unique_100_numbers.sv
@@ -54,4 +54,49 @@ endpackage:my_pkg
           end
         endmodule : tb_top
 
+      //tb top
+
+/*
+Below code is for making sure generating 20 unique values within a given range of 20-50
+
+package my_pkg;
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+class my_cls;
+  //generate unique numbers between a range 20-50 20
+  rand bit[7:0] a_arr[20];
+  
+  constraint a_q_c {
+    foreach(a_arr[i]){
+      a_arr[i] inside {[20:50]};
+      if(i>0){
+        a_arr[i] > a_arr[i-1];
+      }
+    }
+  }
+  
+        function void post_randomize();
+     a_arr.shuffle();
+      endfunction : post_randomize
+  
+endclass : my_cls
+
+endpackage:my_pkg
+        
+        module tb_top;
+          import my_pkg::*;
+          
+          my_cls cls;
+          
+          initial begin
+            cls =new();
+            cls.randomize();
+            foreach(cls.a_arr[i])begin
+              $display("Elements of the array are: %d",cls.a_arr[i]);
+            end
+          end
+        endmodule : tb_top*/
+
 
