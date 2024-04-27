d8a90a9e45374c331eaf794f5ee687f9b9ffcf40
methekaran <92954893+methekaran@users.noreply.github.com>
2024-04-26 19:53:03 -0700

    Update a_b_c_assert.sv

--- a_b_c_assert.sv
+++ a_b_c_assert.sv
@@ -2,6 +2,10 @@
 i. if a is high  b should be high when c is low
 ii. if a is high b should be low when c  is high
 
+//Whenever in doubt for the use of throughout refer to this link:
+//https://www.design-reuse.com/articles/44987/system-verilog-assertions-simplified.html
+
+//This should work I think  
 property p_a_b_c;
   @(posedge clk) disable iff(!rst) $rose(a) |-> b throughout c[->1];
 endproperty : p_a_b_c
