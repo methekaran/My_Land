e1e60d44e66adb7597de9fdc6b1e405d8327b4f0
methekaran <92954893+methekaran@users.noreply.github.com>
2024-04-29 06:17:38 -0700

    Create Req_should_be_low_wgnt.sv

---
+++ Req_should_be_low_wgnt.sv
@@ -0,0 +1,8 @@
+//If REQ happen GNT should happen in the next clock cycle.    REQ |->##1 GNT .    If REQ should never happen while GNT is asserted
+
+//i) once req is asserted gnt should be asserted in next clock cycle
+//ii) if gnt is high req should never be asserted
+
+property req_gnt_not_happen;
+  @(posedge clk) disable iff(!rstn) $rose(REQ) |=> (GNT) throughout !(REQ); 
+endproperty : req_gnt_not_happen
