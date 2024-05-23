2dc76572b018d42a9a02b1e1c1d867734f180c47
methekaran <92954893+methekaran@users.noreply.github.com>
2024-05-23 10:23:12 -0700

    Create count_assert_check.sv

---
+++ count_assert_check.sv
@@ -0,0 +1,16 @@
+//Check if count variable increments everytime signal valid is set
+property p_count_based_valid(count);
+  logic[31:0] local_count;
+  @(posedge clk) disable iff(!rstn) ($rose(valid),local_count=0,local_count=local_count+1) |=> (count===local_count);
+endproperty : p_count_based_valid
+
+//write_data should be stable until write_ack arrives
+property p_write_data_chk;
+  @(posedge clk) disable iff(!rstn) $stable(write_data) throughout write_ack[->1];  
+endproperty : p_write_data_chk
+  
+//If valid is asserted, it should remain high until ready is received
+property p_ready_valid_chk;
+  @(posedge clk) disable iff(!rstn) $rose(valid) |-> (valid===1) throughout ready[->1] ##1 (valid===0);
+endproperty : p_ready_valid_chk
+
