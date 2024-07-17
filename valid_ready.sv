b38e850114a954029f9907c2f655b0b181b661bd
methekaran <92954893+methekaran@users.noreply.github.com>
2024-07-16 19:13:01 -0700

    Update valid_ready.sv

--- valid_ready.sv
+++ valid_ready.sv
@@ -1,6 +1,6 @@
 //Valid should be high until ready is asserted
-
+//valid must be held high until ready is asserted high
 property p_valid_ready;
-   $rose(valid) |-> valid throughout ready[->1] ##1 !(valid);
+  @(posedge clk) disable iff(!rstn) $rose(valid) |-> (valid===1) throughout ready[->1] ##1 (!valid);
 endproperty : p_valid_ready
                                     
