52b7db01e703ce09b1629ff89d357337c6d119fd
methekaran <92954893+methekaran@users.noreply.github.com>
2024-07-20 09:25:21 -0700

    Update two_d_row_col_unique.sv

--- two_d_row_col_unique.sv
+++ two_d_row_col_unique.sv
@@ -1,58 +1,45 @@
 //tb top
 
-package my_pkg;
+//A generation of unique numbers for a 2d array
 
-//Write a constraint for distinct adjacent element (adjacent elements should not be the same) in a 2d array (specifically for the boundary elements size of 2d array is mxm where m can be any integer)
+package my_pkg;
 
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 
-class my_cls;
-  
-  rand bit[3:0] an_array[3][3];
-  
-  constraint an_array_c {
-    //an_array.size() inside {3};
-    //foreach(an_array[i]){
-    //  an_array[i].size()==3;
-    //}
-    
-      foreach(an_array[i,j]){
-        foreach(an_array[,k]){
-          if(j!=k){
-            an_array[i][j] != an_array[i][k];
+      class my_2d_arr;
+        bit[7:0] a_2d_arr[5][5];
+        rand bit[7:0] temp_arr[25];
+        
+        constraint a_2d_arr_c {
+          foreach(temp_arr[i]){
+            if(i>0){
+              temp_arr[i] > temp_arr[i-1];
+            }
           }
         }
-      }
-            foreach(an_array[a,b]){
-              foreach(an_array[c,]){
-                if(a!=c){
-                  an_array[a][b]!=an_array[c][b];
-                }
-              }
-            	
-            }
-  }
-endclass : my_cls
-
-
-
+              
+              function void post_randomize();
+            temp_arr.shuffle();
+            foreach(a_2d_arr[i,j])begin
+              a_2d_arr[i][j] = temp_arr[i*5+j];
+              $display("My 2d arr value is=%0d",a_2d_arr[i][j]);
+            end
+            endfunction : post_randomize
+      endclass : my_2d_arr
+              
 endpackage : my_pkg
 
-module tb_top;
+module tb;
   import my_pkg::*;
   
-  my_cls cls;
+  my_2d_arr md;
   
   initial begin
-    cls = new();
     
-    repeat(1)begin
-      cls.randomize();
-      foreach(cls.an_array[i,j])begin
-        $display("a value generated:%d,i=%d,j=%d",cls.an_array[i][j],i,j);
-      end
-    end
+    md = new();
+    md.randomize();
   end
   
-endmodule : tb_top
+endmodule : tb
+            
