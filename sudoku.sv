1e993c1841a6e00eac924c64495089ff2458081d
methekaran <92954893+methekaran@users.noreply.github.com>
2024-07-21 11:21:10 -0700

    Update sudoku.sv

--- sudoku.sv
+++ sudoku.sv
@@ -1,52 +1,62 @@
-//tb top
+//Based on http://cluelogic.com/2015/02/hidden-gems-of-systemverilog-solving-sudoku/
 
+// Code your testbench here
+// or browse Examples
 package my_pkg;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
 
-//For a 8 bit variable if the past randomization resulted in a odd value, the next randomization should be even with 75% probability else be even with 25% probability. 
-
-//i) 8 bit variable past randomization is stored in post_randomize (3)
-//ii) To decide next randomization it should be even :=75 (even) (odd:=25)
-//iii) Else :=25% probability of generating even (odd :=75)
-
-import uvm_pkg::*;
-`include "uvm_macros.svh"
-
-//5,7,6,4,3,2
-
-class my_cls;
-  
-  rand bit[7:0] even_odd_num;
-  bit[7:0] prev_value;
-  
-  constraint even_odd_num_c {
-    if(prev_value%2!=0){
-      (even_odd_num[0]) dist {0:=75,1:=25};
+  class my_cls;
+    rand int sudoku[9][9];
+    
+    constraint c_sudoku{
+      foreach(sudoku[row,col]){
+        sudoku[row][col] inside {[1:9]};
+      }
+        
+      //every row's element must be unique
+        foreach(sudoku[row,col0]){
+          foreach(sudoku[,col1]){
+            if(col0!=col1){
+              sudoku[row][col0]!=sudoku[row][col1];
+            }
+          }
+        }
+       
+      //every col's element must be unique
+       foreach(sudoku[row0,col]){
+        foreach(sudoku[row1,]){
+          if(row0!=row1){
+            sudoku[row0][col]!=sudoku[row1][col];
+          }
+        }        
+      }
+            
+      //every 3x3 box must be unique
+            foreach(sudoku[row,col]){
+              foreach(sudoku[row1,col1]){
+                if(row/3==row1/3 && col/3==col1/3 && !(row==row1 && col==col1)){
+                  sudoku[row][col]!=sudoku[row1][col1];
+                }
+              }
+            
+            }
     }
-    else {
-      (even_odd_num[0]) dist {0:=25,1:=75};
-    }
-  }
-   function void post_randomize();
-     prev_value = even_odd_num;
-   endfunction : post_randomize
-  
-endclass : my_cls
-
+  endclass : my_cls
 endpackage : my_pkg
+                  
+                  module tb;
+                    import my_pkg::*;
+                    
+                    my_cls cls;
+                    
+                    initial begin
+                      cls=new();
+                      cls.randomize();
+                      foreach(cls.sudoku[i,j])begin
+                        $display("The sudoku values are=%d,row=%d,col=%d",cls.sudoku[i][j],i,j);
+                      end
+                    end
+                  endmodule : tb
 
-module tb_top;
-  import my_pkg::*;
-  
-  my_cls cls;
-  
-  initial begin
-    cls = new();
-    
-    repeat(10)begin
-    cls.randomize();
-      $display("The even_odd number are=%0h, prev_val=%0h",cls.even_odd_num,cls.prev_value);
-    end
-    
-  end
-  
-endmodule : tb_top
+
