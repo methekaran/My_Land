34314e53a33ab9f2c2646152b087d4872e48b163
methekaran <92954893+methekaran@users.noreply.github.com>
2025-01-20 06:30:27 -0800

    Update missing_num.pl

--- missing_num.pl
+++ missing_num.pl
@@ -1,11 +1,35 @@
-## find a missing number
-my_list = [9,6,4,2,3,5,7,0,1]
+## find a missing number from the given array
 
-my_list.sort()
+#Input: nums = [0,1,2,4]
 
-for i in range(len(my_list)):
-  if(i in my_list):
-    continue 
+#Missing numbers from the list of numbers
+#Do a XOR of index & the result plus result ^ element of the array
+#That will be the answer
+
+
+#int res = nums.length;//4
+
+#    for(int i=0; i<nums.length; i++){
+#        res ^= I; 4 ^0=4; 4 ^1 =5; 4^2=6; 4^3=7
+#        res ^= nums[I]; 4^0 =4 5^1=4; 6^2=4; 7^4 0111^0100=0011
+#    }
+#    return res;
+
+#SUM METHOD
+#int len = nums.length;
+#    int sum = (0+len)*(len+1)/2;
+#    for(int i=0; i<len; i++)
+#        sum-=nums[i];
+#    return sum;
+
+#Below solution is using sort method of python 
+#my_list = [9,6,4,2,3,5,7,0,1]
+
+#my_list.sort()
+
+#for i in range(len(my_list)):
+#  if(i in my_list):
+#    continue 
     #print("The index is found\n")
-  else:
-    print(f"The missing number is {i}\n")
+#  else:
+#    print(f"The missing number is {i}\n")
