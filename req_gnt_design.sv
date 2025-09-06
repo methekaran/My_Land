7d77eac6e329efbf2af63347e185fd3e8b6e10ff
methekaran <92954893+methekaran@users.noreply.github.com>
2025-09-05 18:19:24 -0700

    Update req_gnt_design.sv

--- req_gnt_design.sv
+++ req_gnt_design.sv
@@ -165,7 +165,7 @@ module my_dut(my_intf vif);
   endproperty : p_req_gnt_fell
   
   property p_gnt_one_h;
-    @(posedge vif.clk) disable iff(!vif.reset) $rose(vif.req) |=> $onehot(vif.gnt);
+    @(posedge vif.clk) disable iff(!vif.reset) $rose(vif.req) |=> $onehot0(vif.gnt);
   endproperty : p_gnt_one_h
 
   property p_ack_not_if_req;
